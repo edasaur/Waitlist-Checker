ef60f80c0d51c5369a4ea1300cc4528f982d2926
edasaur <ewang_94@yahoo.com>
2013-12-06 16:53:21 -0800

    added checkboxes to to-do list on readme

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Waitlist-Checker
 ================
 
 A waitlist checker for Berkeley Telebears. Currently, it only works for Econ 101B, Spring Semester~
+
+Timer is a module that keeps track of time and runs the checker module every __60__ seconds. When the checker module
+
+Goals:
+	-Be able to place CCN on clipboard when click on popup                [ ]
+	-Be able to take in CCN for correct course to check waitlists for     [ ] 
+	-Be able to send email notifications                                  [ ]
+	-Be able to send text messages                                        [ ]
+	-Be able to check multiple courses for waitlisting                    [ ]
+	-Be able to automatically enroll/waitlist you in courses              [ ]
--- popup.py
+++ popup.py
@@ -1,17 +1,18 @@
 from Tkinter import *
 
-def showNotification(notificationTimeout, textToDisplay):
+def showNotification(pausetime, textToDisplay):
 
     ## Create main window
     root = Tk()
-    Button(root, text=textToDisplay, activebackground="white", bg="white", command=lambda: root.destroy()).pack(side=LEFT)
-
+    w = Button(root, text=textToDisplay, activebackground="blue", bg="red", command=lambda: root.destroy())
+    w.pack(side=LEFT)
+    w.flash()
     root.update_idletasks()
     # Remove window decorations
     root.overrideredirect(1)
     root.wm_attributes("-topmost", 1)
-    timeOut = int(notificationTimeout*1000) # Convert to ms from s
-
+    
+    timeOut = int(pausetime*1000) # Convert to ms from s
     ## Run appliction
     root.after(timeOut,root.destroy)
     root.mainloop()
